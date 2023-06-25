0feebd9cb39a7615aafc972c4ae055433c2524e5
LENOVO <LENOVO@DESKTOP-2MREFRI>
2023-06-26 04:48:44 +0530

    Add: Start Buying a book

--- src/test/java/com/sdb/tdd/CalculateDiscountTest.java
+++ src/test/java/com/sdb/tdd/CalculateDiscountTest.java
@@ -1,6 +1,7 @@
 package com.sdb.tdd;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
@@ -10,6 +11,14 @@ public class CalculateDiscountTest {
 	public void initializeToBuyBook() {
 		CalculateDiscount calculateDiscount = new CalculateDiscount();
 		assertNotNull(calculateDiscount);
+	}
+
+	@Test
+	public void buyingOneBook() {
+		CalculateDiscount calculateDiscount = new CalculateDiscount();
+		String book = "Clean Code (Robert Martin, 2008)";
+		double discount = 0.00;
+		assertEquals(50.0, calculateDiscount.getTotalPrice(discount));
 	}
 
 }
